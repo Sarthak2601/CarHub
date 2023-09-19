813541bb5c627a4fcea1ce465b7cfb249dbf0934
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2023-09-19 14:03:47 +0530

    combobox

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,10 @@
+import { CustomFilterProps } from '@/types'
+import React from 'react'
+
+const CustomFilter = ({title}: CustomFilterProps) => {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default CustomFilter
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,17 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: String; 
-    containerStyles: String; 
+    title: string; 
+    containerStyles: string; 
     handleOnClick?: MouseEventHandler<HTMLButtonElement>; 
     btnType?: 'button' | 'submit'; 
+}
+
+export interface CustomFilterProps {
+    title: string; 
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: string; 
+    setManufacturer: (manufacturer: string) => void;
 }
