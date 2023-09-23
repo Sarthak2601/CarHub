cbf65e9dc755593f1fb917e63b20e6c27077ef46
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2023-09-23 19:33:51 +0530

    custom filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CustomFilter, Hero, SearchBar, CarCard } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
@@ -26,8 +27,8 @@ export default async function Home({searchParams}) {
         <div className='home__filters'>
           <SearchBar />
           <div className='home__filter-container'>
-            <CustomFilter title='fuel'/>
-            <CustomFilter title='year'/>
+            <CustomFilter title='fuel' options={fuels}/>
+            <CustomFilter title='year' options={yearsOfProduction}/>
           </div>
         </div>
 
--- utils/index.ts
+++ utils/index.ts
@@ -39,3 +39,10 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
     return `${url}`; 
 }
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search); 
+    searchParams.set(type, value)
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+    return newPathName; 
+}
